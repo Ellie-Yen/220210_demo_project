69f23c1ecc24200118abcffe95ce449c717946bf
ellie-yen <bc70522@gmail.com>
2022-02-25 13:06:17 +0800

    try to fix

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require('express');
 const path = require('path');
 const app = express();
 
 const API_SERVICE_URL = 'https://od.moi.gov.tw/';
 
-app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', createProxyMiddleware({
   target: API_SERVICE_URL,
   changeOrigin: true
 }));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
