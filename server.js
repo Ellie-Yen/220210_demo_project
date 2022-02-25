b6e633283f357e9240de2cac7b19492757805985
ellie-yen <bc70522@gmail.com>
2022-02-25 11:21:49 +0800

    try to fix proxy problem

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-var path = require('path');
-var express = require('express');
+const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
-var app = express();
+const path = require('path');
+const express = require('express');
+const app = express();
 
+const API_SERVICE_URL = 'https://od.moi.gov.tw/';
+
+app.use('/api', createProxyMiddleware({
+  target: API_SERVICE_URL,
+  changeOrigin: true
+}));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.use(
-  '/api',
-  createProxyMiddleware({
-    target: 'https://od.moi.gov.tw/',
-    changeOrigin: true,
-  })
-);
 app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function() {
