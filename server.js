d506d35dce3824beddfed5d34be11285ca12ba1e
ellie-yen <bc70522@gmail.com>
2022-02-25 16:46:51 +0800

    try to fix

--- server.js
+++ server.js
@@ -7,16 +7,15 @@ const API_SERVICE_URL = 'https://od.moi.gov.tw/';
 
 app.use('/api', createProxyMiddleware({
   target: API_SERVICE_URL,
-  changeOrigin: true
+  changeOrigin: true,
+  secure: false,
+  headers: {
+    connection: 'keep-alive'
+  }
 }));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8080);
 
-// see if app connect to api
-app.get('*', (req, res) => {
-  console.log(req, res);
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-});
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
