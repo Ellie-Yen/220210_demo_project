b7eec2b8dbe8dc30cd255cfd316723282b455041
ellie-yen <bc70522@gmail.com>
2022-02-25 16:15:40 +0800

    add log

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.set('port', process.env.PORT || 8080);
 // see if app connect to api
 app.get('/api', (req, res) => {
   console.log(req, res);
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
