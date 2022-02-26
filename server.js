28d2cf8fb18e26ec34a925e4891187d8bf706393
ellie-yen <bc70522@gmail.com>
2022-02-26 12:02:17 +0800

    add logger try to fix heroku timeout

--- server.js
+++ server.js
@@ -1,70 +1,40 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
 const express = require('express');
 const path = require('path');
 const app = express();
 
-const extendTimeoutMiddleware = (req, res, next) => {
-  const space = ' ';
-  let isFinished = false;
-  let isDataSent = false;
-
-  // Only extend the timeout for API requests
-  if (!req.url.includes('/api')) {
-    next();
-    return;
-  }
-
-  res.once('finish', () => {
-    isFinished = true;
-  });
-
-  res.once('end', () => {
-    isFinished = true;
-  });
-
-  res.once('close', () => {
-    isFinished = true;
-  });
-
-  res.on('data', (data) => {
-    // Look for something other than our blank space to indicate that real
-    // data is now being sent back to the client.
-    if (data !== space) {
-      isDataSent = true;
-    }
-  });
-
-  const waitAndSend = () => {
-    setTimeout(() => {
-      // If the response hasn't finished and hasn't sent any data back....
-      if (!isFinished && !isDataSent) {
-        // Need to write the status code/headers if they haven't been sent yet.
-        if (!res.headersSent) {
-          res.writeHead(202);
-        }
-
-        res.write(space);
-
-        // Wait another 15 seconds
-        waitAndSend();
-      }
-    }, 15000);
-  };
-
-  waitAndSend();
-  next();
-};
-
 const API_SERVICE_URL = 'https://od.moi.gov.tw/';
+onProxyRes: responseInterceptor(async (buffer, proxyRes, req, res) => {
+  // log original request and proxied request info
+  const exchange = `[DEBUG] ${req.method} ${req.path} -> ${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path} [${proxyRes.statusCode}]`;
+  console.log(exchange);
+
+  // log original response
+  console.log(`[DEBUG] original response:\n${buffer.toString('utf8')}`);
+
+  // set response content-type
+  res.setHeader('content-type', 'application/json; charset=utf-8');
+
+  // set response status code
+  res.statusCode = 200;
+
+  // return a complete different response
+  return JSON.stringify({
+    message: 'test'
+  });
+});
+
 app.use('/api', createProxyMiddleware({
   target: API_SERVICE_URL,
   changeOrigin: true,
   secure: false,
   headers: {
     connection: 'keep-alive'
-  }
+  },
+  logLevel: 'debug',
+  onProxyRes
 }));
-app.use(extendTimeoutMiddleware);
+
 app.use(express.static(path.join(__dirname, 'dist')));
 app.set('port', process.env.PORT || 8000);
 
