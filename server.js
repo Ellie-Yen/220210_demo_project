545bf9ff40196534d950bc45ca5e5a0f25873706
ellie-yen <bc70522@gmail.com>
2022-02-28 13:23:30 +0800

    add proxy on server to handle cors request

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var path = require('path');
 var express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 var app = express();
 
+const API_SERVICE_URL = 'https://od.moi.gov.tw/';
+app.use('/api', createProxyMiddleware({
+  target: API_SERVICE_URL,
+  changeOrigin: true,
+  secure: false,
+  headers: {
+    connection: 'keep-alive'
+  },
+  logLevel: 'debug'
+}));
 app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8000);
 
 var server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
